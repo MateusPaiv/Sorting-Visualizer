2dbe4a72be77316b21caaf4dc1cee61c060a6bf1
MateusPaiv <mateusrobes09@hotmail.com>
2023-08-12 20:43:53 -0300

    Finished

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-const n = 10;
+const n = 15;
 const array=[];
 init();
+
+let audioCtx = null;
+
+function playNote(freq){
+    if(audioCtx==null){
+        audioCtx = new AudioContext();
+    }
+    const dur = 0.1;
+    const osc = audioCtx.createOscillator();
+    osc.frequency.value = freq;
+
+    osc.start();
+    osc.stop(audioCtx.currentTime+dur);
+    const node = audioCtx.createGain();
+    node.gain.value = 0.1;
+    osc.connect(node);
+    node.connect(audioCtx.destination);
+}
  
 function init(){
     for(let i =0; i<n;i++){
@@ -9,28 +27,63 @@ function init(){
     showBars();
 }
 function play(){
-    bubbleSort(array);
-    showBars();
-}
-function bubbleSort(array){
-do{
-    var swapped=false;
-    for(let i=1;i<array.length;i++){
-        if(array[i-1]>array[i]){
-            swapped=true;
-            [array[i-1],array[i]]=[array[i],array[i-1]]
-        }
-    }
-}while(swapped)
+   const copy=[...array]
+   const moves = bubbleSort(copy);
+   animate(moves);
+    // showBars();
 }
 
-function showBars(){
+function animate(moves){
+    if(moves.length==0){
+        showBars();
+        return;
+    }
+    const move = moves.shift();
+    const [i,j] =  move.indices;
+
+    if(move.type == "swap"){
+        [array[i], array[j]]= [array[j], array[i]]
+    }
+    playNote(200+array[i]*500);
+    playNote(200+array[j]*500);
+
+    showBars(move);
+    setTimeout(function(){
+        animate(moves);
+    }, 40)
+    
+}
+function bubbleSort(array){
+    const moves = [];
+    do{
+        var swapped=false;
+        for(let i=1;i<array.length;i++){
+            moves.push({indices:[i-1,i],type:"comp"});
+            if(array[i-1]>array[i]){
+                swapped=true;
+                moves.push({indices:[i-1,i],type:"swap"});
+                [array[i-1],array[i]]=[array[i],array[i-1]]
+                
+            }
+          
+        }
+    
+    }while(swapped);
+    return moves;
+}
+
+function showBars(move){
     container.innerHTML="";
     for(let i=0;i<array.length;i++){
         const bar=document.createElement("div")
         bar.style.height=array[i]*100+"%";
         bar.classList.add("bar")
-        bar.style.backgroundColor="black";
+
+        if(move && move.indices.includes(i)){
+            bar.style.backgroundColor = move.type=="swap"?"red":"blue";
+        }
         container.appendChild(bar);
-    }
+        console.log(bar);
+    }   
 }
+ 
